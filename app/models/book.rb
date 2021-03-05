8fcb6005c8484aba383b761393cd6cc605d81234
Lauren McCoy <lm_lutec@outlook.com>
2021-03-05 11:06:28 -0500

    added scraper model and initial data for book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,33 @@
 class Book < ApplicationRecord
+
+        attr_accessor :title, :author, :release_date, :genre, :description
+        
+         @@all = []
+        
+        def initialize(title, author, release_date, genre, description)
+          @title = title 
+          @author = author 
+          @release_date = release_date
+          @genre = genre 
+          @description = description
+          @@all << self 
+        end
+        
+        def self.create_list
+          Scraper.new.combine_info if @@all.empty? 
+      
+          @@all.each_with_index do |book, index|
+            if book.title != nil
+              puts "#{index + 1}: #{book.title}"
+            end 
+          end 
+      
+        end 
+        
+        def self.all 
+          @@all
+        end 
+        
+        
+        
 end
---
+++ app/models/scraper.rb
@@ -0,0 +1,32 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Scraper 
+  
+    def get_site 
+      Nokogiri::HTML(open("https://www.usatoday.com/entertainment/books/best-selling/"))
+    end
+     
+    def info_container 
+      self.get_site.css(".front-booklist-info-container")
+    end
+     
+    def combine_info 
+      self.info_container.collect do |container| 
+           
+          title = container.css(".books-front-meta-title").children[0].text   
+          author = container.css(".books-front-meta-authorInfo").children[0].text 
+          release_date = container.css(".books-front-meta-debut")[0].text.sub("Debuted:", "")
+          genre = container.css(".books-front-meta-genre").text.split("Genre:").pop
+          description = container.css(".books-front-meta-short").children[0].text 
+          
+          Books.new(title, author, release_date, genre, description)  
+        end 
+    end
+    
+  end 
+    
+    
+     
+  
+   
